e8684b7c38b2d642528d50164393a30733244246
yahiro <yahiro07@users.noreply.github.com>
2021-06-30 04:29:08 +0900

    update my keyboards

---
+++ firmware/src/projects/assort/crkbd/rp/config.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#define KERMITE_PROJECT_ID "GCX88nns"
+
+#define KS_NUM_SCAN_SLOTS 48
+
+#define KS_NUM_RGBLEDS 27
+#define KS_RGBLED_PIN GP0
+
+// #define KS_OLED_ROT180
+
+#define KS_RP_SINGLEWIRE_PIN GP1
+
+#define KS_NUM_COLUMNS 6
+#define KS_NUM_ROWS 4
+
+#define KS_COLUMN_PINS \
+  { GP29, GP28, GP27, GP26, GP22, GP20 }
+
+#define KS_ROW_PINS \
+  { GP4, GP5, GP6, GP7 }
---
+++ firmware/src/projects/assort/lily58/atmega/config.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#define KERMITE_PROJECT_ID "LLiJq6YU"
+
+#define KS_NUM_SCAN_SLOTS 60
+
+#define KS_SNGLEWIRE_PIN D2
+
+#define KS_NUM_COLUMNS 6
+#define KS_NUM_ROWS 5
+
+#define KS_COLUMN_PINS \
+  { F6, F7, B1, B3, B2, B6 }
+
+#define KS_ROW_PINS \
+  { C6, D7, E6, B4, B5 }
